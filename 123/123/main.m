0280548bc1ed319f65b8e3da89fccb6da4e2dc30
孙月明 <sunyueming@sunyuemingdeMacBook.local>
2016-05-09 17:41:14 +0800

    123

---
+++ 123/123/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  123
+//
+//  Created by 孙月明 on 16/5/9.
+//  Copyright © 2016年 Sym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
