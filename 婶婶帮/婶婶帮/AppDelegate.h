38ebeeb4802ca3701869f1d49a15d63f4cea6589
孙月明 <sunyueming@sunyuemingdeMacBook.local>
2016-05-10 09:07:14 +0800

    新建项目婶婶帮

---
+++ 婶婶帮/婶婶帮/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  婶婶帮
+//
+//  Created by 孙月明 on 16/5/10.
+//  Copyright © 2016年 Sym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 婶婶帮/婶婶帮/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  婶婶帮
+//
+//  Created by 孙月明 on 16/5/10.
+//  Copyright © 2016年 Sym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
